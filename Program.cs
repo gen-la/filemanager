1c26d284d40e82104c04d03e2fefd54d212e375d
gen-la <147514418+gen-la@users.noreply.github.com>
2024-05-04 17:19:50 +0200

    update
    now the user can choose a letter to sort the words on

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+
 namespace filemanager
 {
     internal class Program
     {
+        static string character = "";
+        static bool checkChar = false;
         static void Main(string[] args)
         {
+            while (!checkChar) // let user type a letter and do so untill only one letter is typed
+            {
+                Console.WriteLine("Type a letter:");
+                character = Console.ReadLine();
+                if (character.Length != 1) // if its not one single letter then ask user to type again
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please type a single letter.");
+                }
+                else checkChar = true; // if its one letter then break the loop and continue
+            }
             // array with words
             string[] arrayOFwords = { "Dog", "PC", "Book", "Cat", "Door", "boot", "Money", "Apple", "day", "diet" };
 
@@ -14,7 +28,7 @@ namespace filemanager
             List<string> filteredWords = Filter(arrayOFwords);
 
             // print the filtered words to the console
-            Console.WriteLine("Words starting with 'D':");
+            Console.WriteLine($"\nWords starting with {character.ToUpper()}:");
             foreach (string word in filteredWords)
             {
                 Console.WriteLine(word);
@@ -23,7 +37,7 @@ namespace filemanager
             // call function to create folder and text file
             string filePath = FolderAndFile(filteredWords);
 
-            Console.WriteLine("Words starting with 'D' are saved in the file: " + filePath);
+            Console.WriteLine($"\nWords starting with {character.ToUpper()} are saved in the file: " + filePath);
         }
 
         static List<string> Filter(string[] words)
@@ -33,7 +47,7 @@ namespace filemanager
             // add all words starting with 'D' to the list
             foreach (string word in words)
             {
-                if (word.StartsWith("D", StringComparison.OrdinalIgnoreCase)) // ignore upper and lower case
+                if (word.StartsWith($"{character}", StringComparison.OrdinalIgnoreCase)) // ignore upper and lower case
                 {
                     filteredWords.Add(word);
                 }
@@ -48,11 +62,11 @@ namespace filemanager
         static string FolderAndFile(List<string> words)
         {
             // create a folder named "D" if it doesn't exist
-            string directoryPath = Path.Combine(Directory.GetCurrentDirectory(), "D");
+            string directoryPath = Path.Combine(Directory.GetCurrentDirectory(), $"{character.ToUpper()}");
             Directory.CreateDirectory(directoryPath);
 
             // create a text file named "D.txt"
-            string filePath = Path.Combine(directoryPath, "D.txt");
+            string filePath = Path.Combine(directoryPath, $"{character.ToUpper()}.txt");
             // write the filtered words to it
             File.WriteAllLines(filePath, words);
 
--- obj/Debug/net8.0/filemanager.AssemblyInfo.cs
+++ obj/Debug/net8.0/filemanager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("filemanager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d3031d8e38b85ef955780f70874ce0c3c237092")]
 [assembly: System.Reflection.AssemblyProductAttribute("filemanager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("filemanager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
